ca52d98c6221c3b4c3e884ea42fc6254c1e1a40b
Duane Edwards <mail@duaneedwards.net>
2015-02-10 08:47:24 +1000

    Bump to v2.4.0.4

--- NHapi20/SharedAssemblyInfo.cs
+++ NHapi20/SharedAssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.4.0.3")]
-[assembly: AssemblyFileVersion("2.4.0.3")]
+[assembly: AssemblyVersion("2.4.0.4")]
+[assembly: AssemblyFileVersion("2.4.0.4")]
