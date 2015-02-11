94c0c18e96571e28b16f031323b9b4d7361e97fb
Duane Edwards <mail@duaneedwards.net>
2015-02-11 11:15:04 +1000

    update build file to build all available HL7 versions in the complete database retarget app.config's connection string to local directory

--- NHapi20/BuildModels.bat
+++ NHapi20/BuildModels.bat
@@ -1,23 +1,35 @@
-set BASE=D:\Projects\NHapi\SourceForge\NHapi20
+set BASE=C:\Checkouts\duane\nHapi_superclean\nhapi\NHapi20
 set MSBUILD=C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe
+set HL7DB=C:\Checkouts\duane\nHapi_superclean\nhapi\NHapi20\hl7_72_HQ.mdb
 cd %BASE%
-d:
+c:
 cd modelgenerator
 cd bin
 cd debug
-modelgenerator.exe /BasePath=%BASE% /Version=2.1 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Z:\projects\hl7\hl7db.mdb;"
+modelgenerator.exe /BasePath=%BASE% /Version=2.1 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
 
-modelgenerator.exe /BasePath=%BASE% /Version=2.2 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Z:\projects\hl7\hl7db.mdb;"
+modelgenerator.exe /BasePath=%BASE% /Version=2.2 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
 
-modelgenerator.exe /BasePath=%BASE% /Version=2.3 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Z:\projects\hl7\hl7db_23.mdb;"
+modelgenerator.exe /BasePath=%BASE% /Version=2.3 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
 
-modelgenerator.exe /BasePath=%BASE% /Version=2.3.1 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Z:\projects\hl7\hl7db.mdb;"
+modelgenerator.exe /BasePath=%BASE% /Version=2.3.1 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
 
-modelgenerator.exe /BasePath=%BASE% /Version=2.4 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Z:\projects\hl7\hl7db.mdb;"
+modelgenerator.exe /BasePath=%BASE% /Version=2.4 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
 
-modelgenerator.exe /BasePath=%BASE% /Version=2.5 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Z:\projects\hl7\hl7db.mdb;"
+modelgenerator.exe /BasePath=%BASE% /Version=2.5 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
 
+modelgenerator.exe /BasePath=%BASE% /Version=2.5.1 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
+
+modelgenerator.exe /BasePath=%BASE% /Version=2.6 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
+
+modelgenerator.exe /BasePath=%BASE% /Version=2.7 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
+
+modelgenerator.exe /BasePath=%BASE% /Version=2.7.1 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
+
+modelgenerator.exe /BasePath=%BASE% /Version=2.8 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
+
+modelgenerator.exe /BasePath=%BASE% /Version=2.8.1 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%HL7DB%;"
 
 cd %BASE%
 %msbuild% Hl7Models.sln
-
+pause
