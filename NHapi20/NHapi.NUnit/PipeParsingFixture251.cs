3d6bae202cc31be4f28fa0c00ae32956d9b54cdc
Duane Edwards <mail@duaneedwards.net>
2015-02-21 08:31:39 +1000

    correct unit test to assert multiple obx's are parsed for a v 2.5.1 message

--- NHapi20/NHapi.NUnit/PipeParsingFixture251.cs
+++ NHapi20/NHapi.NUnit/PipeParsingFixture251.cs
@@ -19,14 +19,17 @@ OBX|3|FT|||This\.br\is\.br\A Test~MoreText~SomeMoreText||||||F";
 		}
 
 		[Test]
-		public void TestOBR5RepeatingValuesMessage()
+		public void TestOBR5RepeatingValuesMessage_DataTypesAndRepetitions()
 		{
 			var parser = new PipeParser();
 			var oru = new ORU_R01();
 			oru = (ORU_R01)parser.Parse(GetMessage());
 
 			int expectedObservationCount = 3;
-			Assert.IsTrue(oru.GetPATIENT_RESULT(0).ORDER_OBSERVATIONRepetitionsUsed == expectedObservationCount, string.Format("Expected 3 OBX repetitions used for this segment, found {0}", expectedObservationCount));
+			int parsedObservations = oru.GetPATIENT_RESULT(0).GetORDER_OBSERVATION(0).OBSERVATIONRepetitionsUsed;
+			bool parsedCorrectNumberOfObservations = parsedObservations == expectedObservationCount;
+			Assert.IsTrue(parsedCorrectNumberOfObservations, string.Format("Expected 3 OBX repetitions used for this segment, found {0}", parsedObservations));
+			
 			foreach (var obs in oru.GetPATIENT_RESULT(0).GetORDER_OBSERVATION(0).GetOBSERVATION().OBX.GetObservationValue())
 			{
 				Assert.IsTrue(obs.Data is FT);
