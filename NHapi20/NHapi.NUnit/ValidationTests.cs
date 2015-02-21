5c35aea07284051ca556e01910ae1976b96e3fb2
Duane Edwards <mail@duaneedwards.net>
2015-02-21 10:16:45 +1000

    Add unit tests around NM field validation
    -- related to issue #1

--- NHapi20/NHapi.NUnit/ValidationTests.cs
+++ NHapi20/NHapi.NUnit/ValidationTests.cs
@@ -44,10 +44,70 @@ OBX|1|SI|||-1||||||F";
 			}, string.Format("Strict validation should throw a {0} when parsing a SI field with a negative value", typeof(DataTypeException).Name));
 		}
 
-		[Test]
-		public void TestStrictValidation_NMFields_ContainOnlyNumbers()
-		{
+		public const string Message_NM_Alpha = @"MSH|^~\&|XPress Arrival||||200610120839||ORU^R01|EBzH1711114101206|P|2.5.1|||AL|||ASCII
+PID|1||1711114||Appt^Test||19720501||||||||||||001020006
+ORC|||||F
+OBR|1|||ehipack^eHippa Acknowlegment|||200610120839|||||||||00002^eProvider^Electronic|||||||||F
+OBX|1|NM|||Hello||||||F";
 
+		public const string Message_NM_Char = @"MSH|^~\&|XPress Arrival||||200610120839||ORU^R01|EBzH1711114101206|P|2.5.1|||AL|||ASCII
+PID|1||1711114||Appt^Test||19720501||||||||||||001020006
+ORC|||||F
+OBR|1|||ehipack^eHippa Acknowlegment|||200610120839|||||||||00002^eProvider^Electronic|||||||||F
+OBX|1|NM|||!@#$||||||F";
+
+		public const string Message_NM_Number = @"MSH|^~\&|XPress Arrival||||200610120839||ORU^R01|EBzH1711114101206|P|2.5.1|||AL|||ASCII
+PID|1||1711114||Appt^Test||19720501||||||||||||001020006
+ORC|||||F
+OBR|1|||ehipack^eHippa Acknowlegment|||200610120839|||||||||00002^eProvider^Electronic|||||||||F
+OBX|1|NM|||10||||||F";
+
+		public const string Message_NM_NegativeNumber = @"MSH|^~\&|XPress Arrival||||200610120839||ORU^R01|EBzH1711114101206|P|2.5.1|||AL|||ASCII
+PID|1||1711114||Appt^Test||19720501||||||||||||001020006
+ORC|||||F
+OBR|1|||ehipack^eHippa Acknowlegment|||200610120839|||||||||00002^eProvider^Electronic|||||||||F
+OBX|1|NM|||-1||||||F";
+
+		public const string Message_NM_Decimal = @"MSH|^~\&|XPress Arrival||||200610120839||ORU^R01|EBzH1711114101206|P|2.5.1|||AL|||ASCII
+PID|1||1711114||Appt^Test||19720501||||||||||||001020006
+ORC|||||F
+OBR|1|||ehipack^eHippa Acknowlegment|||200610120839|||||||||00002^eProvider^Electronic|||||||||F
+OBX|1|NM|||1.5||||||F";
+
+		[Test]
+		[TestCase(new object[] { Message_NM_Alpha, true })]
+		[TestCase(new object[] { Message_NM_Char, true })]
+		[TestCase(new object[] { Message_NM_Number, false })]
+		[TestCase(new object[] { Message_NM_NegativeNumber, false })]
+		[TestCase(new object[] { Message_NM_Decimal, false })]
+		public void TestStrictValidation_NMFields_ValidNumbers(string testMessage, bool shouldThrow)
+		{
+			var parser = new PipeParser();
+			ORU_R01 oru;
+
+			// default validation context should pass with no exceptions
+			oru = (ORU_R01)parser.Parse(testMessage);
+			foreach (var obs in oru.GetPATIENT_RESULT(0).GetORDER_OBSERVATION(0).GetOBSERVATION().OBX.GetObservationValue())
+			{
+				Assert.IsTrue(obs.Data is NM);
+			}
+
+			parser.ValidationContext = new StrictValidation();
+			string message = string.Format("Strict validation {0} throw a {1} when parsing a NM field with alpha values", shouldThrow ? "should" : "should not", typeof(DataTypeException).Name);
+			if (shouldThrow)
+			{
+				Assert.Throws<DataTypeException>(() =>
+				{
+					oru = (ORU_R01) parser.Parse(testMessage);
+				}, message);
+			}
+			else
+			{
+				Assert.DoesNotThrow(() =>
+				{
+					oru = (ORU_R01)parser.Parse(testMessage);
+				}, message);
+			}
 		}
 	}
 }
