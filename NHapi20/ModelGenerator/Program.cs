195e01416749f701cefe56cc6bbddd8f7a0cbc00
Duane Edwards <mail@duaneedwards.net>
2015-02-21 12:20:22 +1000

    ModelGenerator - Cleanup Code

--- NHapi20/ModelGenerator/Program.cs
+++ NHapi20/ModelGenerator/Program.cs
@@ -2,18 +2,13 @@ using Microsoft.Test.CommandLineParsing;
 
 namespace ModelGenerator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            ModelBuilder builder = new ModelBuilder();
-            CommandLineParser.ParseArguments(builder, args);
-            builder.Execute();
-            
-
-            
-        }
-
-
-    }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			ModelBuilder builder = new ModelBuilder();
+			CommandLineParser.ParseArguments(builder, args);
+			builder.Execute();
+		}
+	}
 }
