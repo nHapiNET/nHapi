4e59b5f04c017f43519cce458cba25b2cb689e92
Duane Edwards <mail@duaneedwards.net>
2015-02-21 10:28:59 +1000

    cleanup SI validation test

--- NHapi20/NHapi.NUnit/ValidationTests.cs
+++ NHapi20/NHapi.NUnit/ValidationTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NHapi.Base;
+﻿using NHapi.Base;
 using NHapi.Base.Parser;
 using NHapi.Base.validation.impl;
 using NHapi.Model.V251.Datatype;
@@ -14,23 +10,21 @@ namespace NHapi.NUnit
 	[TestFixture]
 	public class ValidationTests
 	{
-		public string GetNegativeNumberMessage()
-		{
-			return @"MSH|^~\&|XPress Arrival||||200610120839||ORU^R01|EBzH1711114101206|P|2.5.1|||AL|||ASCII
+		public const string Message_SI_NegativeNumber = @"MSH|^~\&|XPress Arrival||||200610120839||ORU^R01|EBzH1711114101206|P|2.5.1|||AL|||ASCII
 PID|1||1711114||Appt^Test||19720501||||||||||||001020006
 ORC|||||F
 OBR|1|||ehipack^eHippa Acknowlegment|||200610120839|||||||||00002^eProvider^Electronic|||||||||F
 OBX|1|SI|||-1||||||F";
-		}
 		
 		[Test]
-		public void TestStrictValidation_NegativeNumber()
+		[TestCase(new object[] { Message_SI_NegativeNumber })]
+		public void TestStrictValidation_NegativeNumber(string message)
 		{
 			var parser = new PipeParser();
 			ORU_R01 oru;
 			
 			// default validation context should pass with no exceptions
-			oru = (ORU_R01) parser.Parse(GetNegativeNumberMessage());
+			oru = (ORU_R01)parser.Parse(message);
 			foreach (var obs in oru.GetPATIENT_RESULT(0).GetORDER_OBSERVATION(0).GetOBSERVATION().OBX.GetObservationValue())
 			{
 				Assert.IsTrue(obs.Data is SI);
@@ -40,7 +34,7 @@ OBX|1|SI|||-1||||||F";
 			parser.ValidationContext = new StrictValidation();
 			Assert.Throws<DataTypeException>(() =>
 			{
-				oru = (ORU_R01)parser.Parse(GetNegativeNumberMessage());
+				oru = (ORU_R01)parser.Parse(message);
 			}, string.Format("Strict validation should throw a {0} when parsing a SI field with a negative value", typeof(DataTypeException).Name));
 		}
 
