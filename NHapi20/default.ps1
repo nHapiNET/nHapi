ac9058a2b7aaa9c06f4d3eb7b1bafe3c450832ed
Duane Edwards <mail@duaneedwards.net>
2015-02-22 11:05:46 +1000

    Add nunit batch file and psake task

--- NHapi20/default.ps1
+++ NHapi20/default.ps1
@@ -1,3 +1,12 @@
+properties {
+    $projectName = "NHapi20"
+    $unitTestAssembly = "NHapi.NUnit.dll"
+    $projectConfig = "Release"
+    $base_dir = resolve-path .\
+    $test_dir = "$base_dir\NHapi.NUnit\bin\$projectConfig"
+    $nunitPath = "$base_dir\packages\NUnit.Runners.2.6.4\tools"
+}
+
 Task Default -depends Build
 
 Task Clean {
@@ -7,7 +16,7 @@ Task Clean {
 
 Task Build -depends Clean {
 	Write-Host "Building Hl7Models.sln" -ForegroundColor Green
-	Exec { msbuild "Hl7Models.sln" /t:Build /p:Configuration=Release /v:quiet} 
+	Exec { msbuild "Hl7Models.sln" /t:Build /p:Configuration=$projectConfig /v:quiet} 
 }
 
 Task BuildModels {
@@ -24,6 +33,12 @@ Task BuildModels {
 #    Exec { ModelGenerator\Bin\Debug\ModelGenerator.exe /BasePath=. /Version=2.7.1 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Checkouts\duane\nHapi_superclean\nhapi\NHapi20\hl7_72_HQ.mdb;"}
 #    Exec { ModelGenerator\Bin\Debug\ModelGenerator.exe /BasePath=. /Version=2.8 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Checkouts\duane\nHapi_superclean\nhapi\NHapi20\hl7_72_HQ.mdb;"}
 #    Exec { ModelGenerator\Bin\Debug\ModelGenerator.exe /BasePath=. /Version=2.8.1 /ConnectionString="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Checkouts\duane\nHapi_superclean\nhapi\NHapi20\hl7_72_HQ.mdb;"}
+}
+
+task Test {
+	exec {
+		& $nunitPath\nunit-console.exe $test_dir\$unitTestAssembly /nologo /nodots /xml=$base_dir\TestResult.xml
+	}
 }
 
 Task Package -depends Build {
