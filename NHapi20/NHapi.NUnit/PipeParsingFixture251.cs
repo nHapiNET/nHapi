c0793649c3dbaa648c3f8306e3d747f7cd0fdb83
Duane Edwards <mail@duaneedwards.net>
2015-04-23 11:46:18 +1000

    add unit test to assert 251 datatypes parse correctly

--- NHapi20/NHapi.NUnit/PipeParsingFixture251.cs
+++ NHapi20/NHapi.NUnit/PipeParsingFixture251.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NHapi.Base.Parser;
 using NHapi.Model.V251.Datatype;
 using NHapi.Model.V251.Message;
@@ -37,6 +38,41 @@ OBX|3|FT|||This\.br\is\.br\A Test~MoreText~SomeMoreText||||||F";
 				Assert.IsTrue(obs.Data is FT);
 			}
 		}
+
+		[TestCase(
+			@"MSH|^~\&|XPress Arrival||||200610120839||ORU^R01|EBzH1711114101206|P|2.5.1|||AL|||ASCII
+PID|1||1711114||Appt^Test||19720501||||||||||||001020006
+ORC|||||F
+OBR|1|||ehipack^eHippa Acknowlegment|||200610120839|||||||||00002^eProvider^Electronic|||||||||F
+OBX|1|DT|||DTValue||||||F
+OBX|2|ST|||STValue||||||F
+OBX|3|TM|||TMValue||||||F"
+		//OBX|4|ID|||IDValue||||||F //Doesn't work
+		//OBX|5|IS|||ISValue||||||F //Doesn't work
+		)]
+		public void Test_251DataTypesParseCorrectly(string message)
+		{
+			var parser = new PipeParser();
+			var oru = new ORU_R01();
+			oru = (ORU_R01)parser.Parse(message);
+
+			int expectedObservationCount = 3;
+			int parsedObservations = oru.GetPATIENT_RESULT(0).GetORDER_OBSERVATION(0).OBSERVATIONRepetitionsUsed;
+			bool parsedCorrectNumberOfObservations = parsedObservations == expectedObservationCount;
+			Assert.IsTrue(parsedCorrectNumberOfObservations,
+				string.Format("Expected {1} OBX repetitions used for this segment, found {0}", parsedObservations, expectedObservationCount));
+
+			int index = 0;
+			var obs = oru.GetPATIENT_RESULT(0).GetORDER_OBSERVATION(0).GetOBSERVATION(index).OBX.GetObservationValue().FirstOrDefault();
+			Assert.IsTrue(obs.Data is DT);
+			index++;
+			obs = oru.GetPATIENT_RESULT(0).GetORDER_OBSERVATION(0).GetOBSERVATION(index).OBX.GetObservationValue().FirstOrDefault();
+			Assert.IsTrue(obs.Data is ST);
+			index++;
+			obs = oru.GetPATIENT_RESULT(0).GetORDER_OBSERVATION(0).GetOBSERVATION(index).OBX.GetObservationValue().FirstOrDefault();
+			Assert.IsTrue(obs.Data is TM);
+		}
+
 
 		[Test]
 		public void TestAdtA04AndA01MessageStructure()
