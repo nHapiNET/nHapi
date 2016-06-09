799fc999b26575e83747f30940fb834506ac011e
Duane Edwards <mail@duaneedwards.net>
2016-06-09 18:15:21 +1000

    missing from previous commit

---
+++ NHapi20/NHapi.NUnit/SourceGenerationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ModelGenerator;
+using NUnit.Framework;
+
+namespace NHapi.NUnit
+{
+	[TestFixture]
+	public class SourceGenerationTests
+	{
+		[Test, Explicit]
+		public void Test_Generate_251()
+		{
+			var builder = new ModelBuilder();
+			builder.BasePath = @"D:\Checkouts\duane\nHapi_superduperclean_codegen\nhapi\NHapi20";
+			builder.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\hl7_72_HQ.mdb;";
+			builder.MessageTypeToBuild = ModelBuilder.MessageType.EventMapping;
+			builder.Version = "2.5.1";
+
+			builder.Execute();
+		}
+	}
+}
