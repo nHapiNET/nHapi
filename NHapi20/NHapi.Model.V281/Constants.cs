d0880218408b5fccca9ee05ebfc41cb6f15f92de
Duane Edwards <mail@duaneedwards.net>
2016-07-12 17:22:40 +1000

    wip create placeholder projects for 2.7, 2.7.1, 2.8 and 2.8.1

---
+++ NHapi20/NHapi.Model.V281/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHapi.Model.V281
+{
+    public class Constants
+    {
+        public const string VERSION = "2.8.1";
+    }
+}
