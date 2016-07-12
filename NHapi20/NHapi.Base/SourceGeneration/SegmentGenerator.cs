11c35df21ef62be96b0e45022f630c673acb8bdc
Duane Edwards <mail@duaneedwards.net>
2016-07-12 12:50:52 +1000

    Resolve issue whereby disposing of the database connection would result in the segment generator failing for subsequent runs

--- NHapi20/NHapi.Base/SourceGeneration/SegmentGenerator.cs
+++ NHapi20/NHapi.Base/SourceGeneration/SegmentGenerator.cs
@@ -141,72 +141,70 @@ namespace NHapi.Base.SourceGeneration
 				ArrayList elements = new ArrayList();
 				SegmentElement se;
 				String segDesc = null;
-				using (OleDbConnection conn = NormativeDatabase.Instance.Connection)
+				OleDbConnection conn = NormativeDatabase.Instance.Connection;
+				StringBuilder sql = new StringBuilder();
+				sql.Append("SELECT HL7SegmentDataElements.seg_code, HL7SegmentDataElements.seq_no, ");
+				sql.Append("HL7SegmentDataElements.repetitional, HL7SegmentDataElements.repetitions, ");
+				sql.Append("HL7DataElements.description, HL7DataElements.length_old, HL7DataElements.table_id, ");
+				sql.Append("HL7SegmentDataElements.req_opt, HL7Segments.description, HL7DataElements.data_structure ");
+				sql.Append(
+					"FROM HL7Versions RIGHT JOIN (HL7Segments INNER JOIN (HL7DataElements INNER JOIN HL7SegmentDataElements ");
+				sql.Append("ON (HL7DataElements.version_id = HL7SegmentDataElements.version_id) ");
+				sql.Append("AND (HL7DataElements.data_item = HL7SegmentDataElements.data_item)) ");
+				sql.Append("ON (HL7Segments.version_id = HL7SegmentDataElements.version_id) ");
+				sql.Append("AND (HL7Segments.seg_code = HL7SegmentDataElements.seg_code)) ");
+				sql.Append("ON (HL7Versions.version_id = HL7Segments.version_id) ");
+				sql.Append("WHERE HL7SegmentDataElements.seg_code = '");
+				sql.Append(name);
+				sql.Append("' and HL7Versions.hl7_version = '");
+				sql.Append(version);
+				sql.Append("' ORDER BY HL7SegmentDataElements.seg_code, HL7SegmentDataElements.seq_no;");
+				OleDbCommand stmt = SupportClass.TransactionManager.manager.CreateStatement(conn);
+				OleDbCommand temp_OleDbCommand;
+				temp_OleDbCommand = stmt;
+				temp_OleDbCommand.CommandText = sql.ToString();
+				OleDbDataReader rs = temp_OleDbCommand.ExecuteReader();
+
+				while (rs.Read())
 				{
-					StringBuilder sql = new StringBuilder();
-					sql.Append("SELECT HL7SegmentDataElements.seg_code, HL7SegmentDataElements.seq_no, ");
-					sql.Append("HL7SegmentDataElements.repetitional, HL7SegmentDataElements.repetitions, ");
-					sql.Append("HL7DataElements.description, HL7DataElements.length_old, HL7DataElements.table_id, ");
-					sql.Append("HL7SegmentDataElements.req_opt, HL7Segments.description, HL7DataElements.data_structure ");
-					sql.Append(
-						"FROM HL7Versions RIGHT JOIN (HL7Segments INNER JOIN (HL7DataElements INNER JOIN HL7SegmentDataElements ");
-					sql.Append("ON (HL7DataElements.version_id = HL7SegmentDataElements.version_id) ");
-					sql.Append("AND (HL7DataElements.data_item = HL7SegmentDataElements.data_item)) ");
-					sql.Append("ON (HL7Segments.version_id = HL7SegmentDataElements.version_id) ");
-					sql.Append("AND (HL7Segments.seg_code = HL7SegmentDataElements.seg_code)) ");
-					sql.Append("ON (HL7Versions.version_id = HL7Segments.version_id) ");
-					sql.Append("WHERE HL7SegmentDataElements.seg_code = '");
-					sql.Append(name);
-					sql.Append("' and HL7Versions.hl7_version = '");
-					sql.Append(version);
-					sql.Append("' ORDER BY HL7SegmentDataElements.seg_code, HL7SegmentDataElements.seq_no;");
-					OleDbCommand stmt = SupportClass.TransactionManager.manager.CreateStatement(conn);
-					OleDbCommand temp_OleDbCommand;
-					temp_OleDbCommand = stmt;
-					temp_OleDbCommand.CommandText = sql.ToString();
-					OleDbDataReader rs = temp_OleDbCommand.ExecuteReader();
+					if (segDesc == null)
+						segDesc = Convert.ToString(rs[9 - 1]);
+					se = new SegmentElement();
+					se.field = Convert.ToInt32(rs.GetValue(2 - 1));
+					se.rep = Convert.ToString(rs[3 - 1]);
+					if (rs.IsDBNull(4 - 1))
+						se.repetitions = 0;
+					else
+						se.repetitions = Convert.ToInt32(rs.GetValue(4 - 1));
 
-					while (rs.Read())
+					if (se.repetitions == 0)
 					{
-						if (segDesc == null)
-							segDesc = Convert.ToString(rs[9 - 1]);
-						se = new SegmentElement();
-						se.field = Convert.ToInt32(rs.GetValue(2 - 1));
-						se.rep = Convert.ToString(rs[3 - 1]);
-						if (rs.IsDBNull(4 - 1))
-							se.repetitions = 0;
-						else
-							se.repetitions = Convert.ToInt32(rs.GetValue(4 - 1));
-
-						if (se.repetitions == 0)
+						if (se.rep == null || !se.rep.ToUpper().Equals("Y".ToUpper()))
 						{
-							if (se.rep == null || !se.rep.ToUpper().Equals("Y".ToUpper()))
-							{
-								se.repetitions = 1;
-							}
+							se.repetitions = 1;
 						}
-						se.desc = Convert.ToString(rs[5 - 1]);
-						if (!rs.IsDBNull(6 - 1))
-						{
-							se.length = DetermineLength(rs);
-						}
-
-						se.table = Convert.ToInt32(rs.GetValue(7 - 1));
-						se.opt = Convert.ToString(rs[8 - 1]);
-						se.type = Convert.ToString(rs[10 - 1]);
-						//shorten CE_x to CE
-						if (se.type.StartsWith("CE"))
-							se.type = "CE";
-
-						elements.Add(se);
-						/*System.out.println("Segment: " + name + " Field: " + se.field + " Rep: " + se.rep +
-                        " Repetitions: " + se.repetitions + " Desc: " + se.desc + " Length: " + se.length +
-                        " Table: " + se.table + " Segment Desc: " + segDesc);*/
 					}
-					rs.Close();
-					stmt.Dispose();
-					NormativeDatabase.Instance.returnConnection(conn);
+					se.desc = Convert.ToString(rs[5 - 1]);
+					if (!rs.IsDBNull(6 - 1))
+					{
+						se.length = DetermineLength(rs);
+					}
+
+					se.table = Convert.ToInt32(rs.GetValue(7 - 1));
+					se.opt = Convert.ToString(rs[8 - 1]);
+					se.type = Convert.ToString(rs[10 - 1]);
+					//shorten CE_x to CE
+					if (se.type.StartsWith("CE"))
+						se.type = "CE";
+
+					elements.Add(se);
+					/*System.out.println("Segment: " + name + " Field: " + se.field + " Rep: " + se.rep +
+							" Repetitions: " + se.repetitions + " Desc: " + se.desc + " Length: " + se.length +
+							" Table: " + se.table + " Segment Desc: " + segDesc);*/
 				}
+				rs.Close();
+				stmt.Dispose();
+				NormativeDatabase.Instance.returnConnection(conn);
 
 				//write imports, class documentation, etc ...
 				source.Append("using System;\r\n");
