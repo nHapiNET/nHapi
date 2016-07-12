fbecfc172d188e3fadf946c54b856405e8cc4cc8
Duane Edwards <mail@duaneedwards.net>
2016-07-12 15:49:09 +1000

    wip create (not implemented) ARV and UAC datatypes

---
+++ NHapi20/NHapi.Model.V26/Datatype/ARV.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHapi.Model.V26.Datatype
+{
+	public class ARV
+	{
+		public ARV()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ NHapi20/NHapi.Model.V26/Datatype/UAC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHapi.Model.V26.Datatype
+{
+	public class UAC
+	{
+		public UAC()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
