aea5d10937e6228b50920e4da949ac04c7fe81e1
Duane Edwards <mail@duaneedwards.net>
2016-07-12 17:01:44 +1000

    Regenerate v2.7.1

--- NHapi20/NHapi.Model.V271/Message/RDY_K15.cs
+++ NHapi20/NHapi.Model.V271/Message/RDY_K15.cs
@@ -2,6 +2,7 @@ using System;
 using NHapi.Base.Log;
 using NHapi.Model.V271.Group;
 using NHapi.Model.V271.Segment;
+using NHapi.Model.V271.Datatype;
 using NHapi.Base;
 using NHapi.Base.Parser;
 using NHapi.Base.Model;
