0616a7681db43b11314d438473bca2bed8f6d6d2
Duane Edwards <mail@duaneedwards.net>
2016-07-12 17:01:19 +1000

    Regenerate v2.8

--- NHapi20/NHapi.Model.V28/Message/RSP_K23.cs
+++ NHapi20/NHapi.Model.V28/Message/RSP_K23.cs
@@ -2,6 +2,7 @@ using System;
 using NHapi.Base.Log;
 using NHapi.Model.V28.Group;
 using NHapi.Model.V28.Segment;
+using NHapi.Model.V28.Datatype;
 using NHapi.Base;
 using NHapi.Base.Parser;
 using NHapi.Base.Model;
