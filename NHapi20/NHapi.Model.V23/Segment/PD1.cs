99d1b600a3ab28f2872637e8d69d8aad4d226e50
Duane Edwards <mail@duaneedwards.net>
2016-07-14 15:35:45 +1000

    BREAKING CHANGE PD1|1 "Living Dependency" is meant to be repeating

--- NHapi20/NHapi.Model.V23/Segment/PD1.cs
+++ NHapi20/NHapi.Model.V23/Segment/PD1.cs
@@ -39,7 +39,7 @@ public class PD1 : AbstractSegment  {
 	public PD1(IGroup parent, IModelClassFactory factory) : base(parent,factory) {
 	IMessage message = Message;
     try {
-       this.add(typeof(IS), false, 1, 2, new System.Object[]{message, 223}, "Living Dependency");
+       this.add(typeof(IS), false, 0, 2, new System.Object[]{message, 223}, "Living Dependency");
        this.add(typeof(IS), false, 1, 2, new System.Object[]{message, 220}, "Living Arrangement");
        this.add(typeof(XON), false, 0, 90, new System.Object[]{message}, "Patient Primary Facility");
        this.add(typeof(XCN), false, 0, 90, new System.Object[]{message}, "Patient Primary Care Provider Name and ID No.");
@@ -57,28 +57,63 @@ public class PD1 : AbstractSegment  {
   }
 
 	///<summary>
-	/// Returns Living Dependency(PD1-1).
+	/// Returns a single repetition of Living Dependency(PD1-1).
+	/// throws HL7Exception if the repetition number is invalid.
+	/// <param name="rep">The repetition number (this is a repeating field)</param>
 	///</summary>
-	public IS LivingDependency
+	public IS GetLivingDependency(int rep)
 	{
-		get{
 			IS ret = null;
 			try
 			{
-			IType t = this.GetField(1, 0);
+			IType t = this.GetField(1, rep);
 				ret = (IS)t;
-			}
-			 catch (HL7Exception he) {
-			HapiLogFactory.GetHapiLog(GetType()).Error("Unexpected problem obtaining field value.  This is a bug.", he);
-				throw new System.Exception("An unexpected error ocurred", he);
 		} catch (System.Exception ex) {
 			HapiLogFactory.GetHapiLog(GetType()).Error("Unexpected problem obtaining field value.  This is a bug.", ex);
 				throw new System.Exception("An unexpected error ocurred", ex);
     }
 			return ret;
-	}
   }
 
+  ///<summary>
+  /// Returns all repetitions of Living Dependency (PD1-1).
+   ///</summary>
+  public IS[] GetLivingDependency() {
+     IS[] ret = null;
+    try {
+        IType[] t = this.GetField(1);  
+        ret = new IS[t.Length];
+        for (int i = 0; i < ret.Length; i++) {
+            ret[i] = (IS)t[i];
+        }
+    } catch (HL7Exception he) {
+        HapiLogFactory.GetHapiLog(this.GetType()).Error("Unexpected problem obtaining field value.  This is a bug.", he);
+        throw new System.Exception("An unexpected error ocurred", he);
+    } catch (System.Exception cce) {
+        HapiLogFactory.GetHapiLog(GetType()).Error("Unexpected problem obtaining field value.  This is a bug.", cce);
+        throw new System.Exception("An unexpected error ocurred", cce);
+  }
+ return ret;
+}
+
+  ///<summary>
+  /// Returns the total repetitions of Living Dependency (PD1-1).
+   ///</summary>
+  public int LivingDependencyRepetitionsUsed
+{
+get{
+    try {
+	return GetTotalFieldRepetitionsUsed(1);
+    }
+catch (HL7Exception he) {
+        HapiLogFactory.GetHapiLog(this.GetType()).Error("Unexpected problem obtaining field value.  This is a bug.", he);
+        throw new System.Exception("An unexpected error ocurred", he);
+} catch (System.Exception cce) {
+        HapiLogFactory.GetHapiLog(GetType()).Error("Unexpected problem obtaining field value.  This is a bug.", cce);
+        throw new System.Exception("An unexpected error ocurred", cce);
+}
+}
+}
 	///<summary>
 	/// Returns Living Arrangement(PD1-2).
 	///</summary>
