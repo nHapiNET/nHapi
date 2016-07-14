3952345eb6997a1c882c5bfbb8d89e2b129f5d58
Duane Edwards <mail@duaneedwards.net>
2016-07-14 13:38:00 +1000

    Regenerate the v271 Groups to include the NHapi.Model.V271.Datatype namespace

--- NHapi20/NHapi.Model.V271/Group/NMD_N02_CLOCK_AND_STATS_WITH_NOTES.cs
+++ NHapi20/NHapi.Model.V271/Group/NMD_N02_CLOCK_AND_STATS_WITH_NOTES.cs
@@ -3,7 +3,7 @@ using NHapi.Base;
 using NHapi.Base.Log;
 using System;
 using NHapi.Model.V271.Segment;
-
+using NHapi.Model.V271.Datatype;
 using NHapi.Base.Model;
 
 namespace NHapi.Model.V271.Group
