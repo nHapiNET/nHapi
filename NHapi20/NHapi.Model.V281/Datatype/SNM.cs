64556f544f89481f74a23da8362dcf81d2048904
Duane Edwards <mail@duaneedwards.net>
2016-07-14 14:10:35 +1000

    Manually implement SNM class based off of ST for v281

---
+++ NHapi20/NHapi.Model.V281/Datatype/SNM.cs
@@ -0,0 +1,38 @@
+using System;
+
+using NHapi.Base.Model;
+namespace NHapi.Model.V281.Datatype
+{
+/// <summary>/// Summary description for SNM.
+/// </summary>
+public class SNM : AbstractPrimitive
+{
+/// <summary>Return the version
+/// <returns>2.8.1</returns>
+///</summary>
+
+            virtual public System.String Version
+            {
+			    get
+			    {
+				    return "2.8.1";
+			    }
+		    }
+            
+
+
+                ///<summary>Construct the type
+                ///<param name="theMessage">message to which this Type belongs</param>
+                ///</summary>
+                public SNM(IMessage theMessage):base(theMessage)
+                {}
+                
+
+
+                ///<summary>Construct the type
+                ///<param name="message">message to which this Type belongs</param>
+                ///<param name="description">The description of this type</param>
+                ///</summary>
+		        public SNM(IMessage message, string description) : base(message,description)
+    	        {}
+                }}
