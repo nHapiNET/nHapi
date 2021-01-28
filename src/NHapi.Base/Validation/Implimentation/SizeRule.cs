3ce7909ec34b4c42bb28f0389ea6998b15a2826c
Tristan Wilson <tristanwilson111@gmail.com>
2021-01-28 16:33:00 -0500

    Fix spelling mistake

--- src/NHapi.Base/Validation/Implimentation/SizeRule.cs
+++ src/NHapi.Base/Validation/Implimentation/SizeRule.cs
@@ -10,7 +10,7 @@
 /// 2004.  All Rights Reserved. 
 /// Contributor(s): ______________________________________. 
 /// Alternatively, the contents of this file may be used under the terms of the 
-/// GNU General Public License (the  ìGPLî), in which case the provisions of the GPL are 
+/// GNU General Public License (the  ‚ÄúGPL‚Äù), in which case the provisions of the GPL are 
 /// applicable instead of those above.  If you wish to allow use of your version of this 
 /// file only under the terms of the GPL and not to allow others to use your version 
 /// of this file under the MPL, indicate your decision by deleting  the provisions above 
@@ -37,7 +37,7 @@ namespace NHapi.Base.validation.impl
 		/// </seealso>
 		public virtual String Description
 		{
-			get { return "Maxumim size <= " + myMaxChars + " characters"; }
+			get { return "Maximum size <= " + myMaxChars + " characters"; }
 		}
 
 		/// <seealso cref="NHapi.Base.validation.Rule.getSectionReference()">
