073b49200f28334639921dbc9bee4c2b444a5836
Alex McNair <alex.mcnair@hotmail.co.uk>
2021-01-29 10:49:47 +0000

    128 > NHapi.Base > Formatted XML License Comments - Updated licence comment format

--- src/NHapi.Base/Model/Primitive/CommonDT.cs
+++ src/NHapi.Base/Model/Primitive/CommonDT.cs
@@ -1,53 +1,53 @@
-/* <summary> The contents of this file are subject to the Mozilla Public License Version 1.1
-/// (the "License"); you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at http://www.mozilla.org/MPL/
-/// Software distributed under the License is distributed on an "AS IS" basis,
-/// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
-/// specific language governing rights and limitations under the License.
-/// 
-/// The Original Code is "CommonDT.java".  Description:
-/// "Note: The class description below has been excerpted from the Hl7 2.4 documentation"
-/// 
-/// The Initial Developer of the Original Code is University Health Network. Copyright (C)
-/// 2001.  All Rights Reserved.
-/// 
-/// Contributor(s): ______________________________________.
-/// 
-/// Alternatively, the contents of this file may be used under the terms of the
-/// GNU General Public License (the  “GPL”), in which case the provisions of the GPL are
-/// applicable instead of those above.  If you wish to allow use of your version of this
-/// file only under the terms of the GPL and not to allow others to use your version
-/// of this file under the MPL, indicate your decision by deleting  the provisions above
-/// and replace  them with the notice and other provisions required by the GPL License.
-/// If you do not delete the provisions above, a recipient may use your version of
-/// this file under either the MPL or the GPL.
-/// 
+/*
+  The contents of this file are subject to the Mozilla Public License Version 1.1
+  (the "License"); you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at http://www.mozilla.org/MPL/
+  Software distributed under the License is distributed on an "AS IS" basis,
+  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+  specific language governing rights and limitations under the License.
+  
+  The Original Code is "CommonDT.java".  Description:
+  "Note: The class description below has been excerpted from the Hl7 2.4 documentation"
+  
+  The Initial Developer of the Original Code is University Health Network. Copyright (C)
+  2001.  All Rights Reserved.
+  
+  Contributor(s): ______________________________________.
+  
+  Alternatively, the contents of this file may be used under the terms of the
+  GNU General Public License (the "GPL"), in which case the provisions of the GPL are
+  applicable instead of those above.  If you wish to allow use of your version of this
+  file only under the terms of the GPL and not to allow others to use your version
+  of this file under the MPL, indicate your decision by deleting  the provisions above
+  and replace  them with the notice and other provisions required by the GPL License.
+  If you do not delete the provisions above, a recipient may use your version of
+  this file under either the MPL or the GPL.
 */
 
 using System;
 using System.Globalization;
-using NHapi.Base.Model;
+
 using NHapi.Base.Log;
 
 namespace NHapi.Base.Model.Primitive
 {
-	/// <summary> This class contains functionality used by the DT class
-	/// in the version 2.3.0, 2.3.1, and 2.4 packages
-	/// 
-	/// Note: The class description below has been excerpted from the Hl7 2.4 documentation. Sectional
-	/// references made below also refer to the same documentation.
-	/// 
-	/// Format: YYYY[MM[DD]]
-	/// In prior versions of HL7, this data type was always specified to be in the format YYYYMMDD. In the current and future
-	/// versions, the precision of a date may be expressed by limiting the number of digits used with the format specification
-	/// YYYY[MM[DD]]. Thus, YYYY is used to specify a precision of "year," YYYYMM specifies a precision of "month,"
-	/// and YYYYMMDD specifies a precision of "day."
-	/// By site-specific agreement, YYYYMMDD may be used where backward compatibility must be maintained.
-	/// Examples:   |19880704|  |199503|
-	/// </summary>
-	/// <author>  Neal Acharya
-	/// </author>
-	public class CommonDT
+   /// <summary> This class contains functionality used by the DT class
+   /// in the version 2.3.0, 2.3.1, and 2.4 packages
+   /// 
+   /// Note: The class description below has been excerpted from the Hl7 2.4 documentation. Sectional
+   /// references made below also refer to the same documentation.
+   /// 
+   /// Format: YYYY[MM[DD]]
+   /// In prior versions of HL7, this data type was always specified to be in the format YYYYMMDD. In the current and future
+   /// versions, the precision of a date may be expressed by limiting the number of digits used with the format specification
+   /// YYYY[MM[DD]]. Thus, YYYY is used to specify a precision of "year," YYYYMM specifies a precision of "month,"
+   /// and YYYYMMDD specifies a precision of "day."
+   /// By site-specific agreement, YYYYMMDD may be used where backward compatibility must be maintained.
+   /// Examples:   |19880704|  |199503|
+   /// </summary>
+   /// <author>  Neal Acharya
+   /// </author>
+   public class CommonDT
 	{
 		/// <summary> Returns the HL7 DT string value.</summary>
 		/// <summary> This method takes in a string HL7 date value and performs validations
@@ -110,18 +110,18 @@ namespace NHapi.Base.Model.Primitive
 						//validations are complete now store the input value into the private value field
 						value_Renamed = value;
 					}
-						//end try
+					//end try
 					catch (DataTypeException e)
 					{
 						throw e;
 					}
-						//end catch
+					//end catch
 					catch (Exception e)
 					{
 						throw new DataTypeException("An unexpected exception ocurred", e);
 					} //end catch
 				}
-					//end if
+				//end if
 				else
 				{
 					//set the private value field to null or empty space.
@@ -157,12 +157,12 @@ namespace NHapi.Base.Model.Primitive
 					day = 0;
 					value_Renamed = Convert.ToString(value);
 				}
-					//end try
+				//end try
 				catch (DataTypeException e)
 				{
 					throw e;
 				}
-					//end catch
+				//end catch
 				catch (Exception e)
 				{
 					throw new DataTypeException("An unexpected exception ocurred", e);
@@ -317,7 +317,7 @@ namespace NHapi.Base.Model.Primitive
 
 		static CommonDT()
 		{
-			log = HapiLogFactory.GetHapiLog(typeof (CommonDT));
+			log = HapiLogFactory.GetHapiLog(typeof(CommonDT));
 		}
 	} //end class
 }
