ab256f7992c7cfb9c74c8ac94d664a2f61824d0d
Jake Aitchison <jake.aitchison@answerdigital.com>
2021-11-22 11:42:55 +0000

    Add unit test for #65

--- tests/NHapi.NUnit/Parser/PipeParserV251Tests.cs
+++ tests/NHapi.NUnit/Parser/PipeParserV251Tests.cs
@@ -206,5 +206,55 @@
             var secondObr = secondOrder.OBSERVATION_REQUEST.OBR.UniversalServiceIdentifier;
             Assert.AreEqual("TT2", secondObr.Identifier.Value);
         }
+
+        /// <summary>
+        /// https://github.com/nHapiNET/nHapi/issues/65.
+        /// </summary>
+        [Test]
+        public void ParseORDERGroupRepetitionsIn_OML_O21()
+        {
+            const string message =
+                "MSH|^~\\&|PracticeFusion|1|||20130903193451+0000||OML^O21^OML_O21|PFOMSGID999999999|T|2.5.1|||AL|NE|||||ELINCS_MT-OML-1_1.0\r"
+                + "PID|1||JD256960^^^^PT~b79a936f-eefb-4d39-8a8f-09ab61a8d6b4^^^^PI||Test^Patient^M||20040229|M|||10 Main St^^San Francisco^CA^94100||^^^patientemail@email.com^^555^5555555~^^^^^555^5555555\r"
+                + "PV1|1|U||||||||||||||||||T\r"
+                + "IN1|1|Other|47198|Anthem Blue Cross|10 Main St^^Fairfax^CA^94930|||GRP100|||Acme Co|||||Doe^Jane^O|SPO^Spouse^HL70063|20000410|10 Main St^^San Francisco^CA^94100|||||||||||||||||887766\r"
+                + "GT1|1||Doe^John^M||10 Main St^^San Francisco^CA^94100|||||P\r"
+                + "ORC|NW|1403R8NY||1403R8NY||||||||1234567893^Provider^Test^^^^^^^^^^NPI||^^^^^555^5555555||||||||||420 Taylor St^^San Francisco^CA^94102\r"
+                + "TQ1|1||||||||S\r"
+                + "OBR|1|PF-13-00011||0029-9^ACID FAST BACILLI STAIN,SPUTUM^99BIO^^^LN|||20130815110800+0000||||O|||||||||RO\r"
+                + "NTE|1||Here is note with special characters like \\& and \\~ and even \\\\| just for you\r"
+                + "DG1|1||800.3^Closed fracture of vault of skull with other and unspecified intracranial hemorrhage^I9C|||F\r"
+                + "DG1|2||601.1^Chronic prostatitis^I9C|||F\r"
+                + "ORC|NW|1403R8NY||PF-13-00011||||||||1234567893^Provider^Test^^^^^^^^^^NPI||^^^^^555^5555555||||||||||420 Taylor St^^San Francisco^CA^94102\r"
+                + "OBR|2|PF-13-00011||0004-2^CHEM 12 PROFILE^99BIO^^^LN|||20130820111100+0000||||O|||||||||RO\r"
+                + "DG1|1||552^Other hernia of abdominal cavity, with obstruction, but without mention of gangrene^I9C|||F\r"
+                + "DG1|2||350.2^Atypical face pain^I9C|||F\r"
+                + "DG1|3||352.3^Disorders of pneumogastric [10th] nerve^I9C|||F\r"
+                + "DG1|4||401^Essential hypertension^I9C|||F\r"
+                + "OBX|1|ST|How are you feeling?^How are you feeling?^99BIO||True||||||F\r"
+                + "ORC|NW|1403R8NY||1403R8NY||||||||1234567893^Provider^Test^^^^^^^^^^NPI||^^^^^555^5||||||||||420 Taylor St^^San Francisco^CA^94102\r"
+                + "OBR|3|1403R8NY||1165-0^ANTI-JO^99BIO^^^LN|||20130814111300+0000||||O|||||||||RO\r"
+                + "NTE|1||And another note\r"
+                + "DG1|1||666^Postpartum hemorrhage^I9C|||F";
+
+            var parser = new PipeParser();
+            var oml = parser.Parse(message, new ParserOptions { NonGreedyMode = true }) as OML_O21;
+
+            Assert.NotNull(oml);
+
+            Assert.AreEqual(3, oml.ORDERRepetitionsUsed);
+
+            var firstOrder = oml.ORDERs.ElementAt(0);
+            var firstObr = firstOrder.OBSERVATION_REQUEST.OBR.UniversalServiceIdentifier;
+            Assert.AreEqual("0029-9", firstObr.Identifier.Value);
+
+            var secondOrder = oml.ORDERs.ElementAt(1);
+            var secondObr = secondOrder.OBSERVATION_REQUEST.OBR.UniversalServiceIdentifier;
+            Assert.AreEqual("0004-2", secondObr.Identifier.Value);
+
+            var thirdOrder = oml.ORDERs.ElementAt(2);
+            var thirdObr = thirdOrder.OBSERVATION_REQUEST.OBR.UniversalServiceIdentifier;
+            Assert.AreEqual("1165-0", thirdObr.Identifier.Value);
+        }
     }
 }
